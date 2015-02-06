b187dad9e6c1b1967bcc8ac4d5bd220116839369
Pierre Guivarch <pierre@crestandco.com>
2015-02-06 09:27:03 -0500

    Add conversion method

--- main/Amount.py
+++ main/Amount.py
@@ -1,6 +1,17 @@
 #Or MonetaryValue
+import logging
+
+logger = logging.getLogger('root') 
 class Amount(object):
     AVAILABLE_CURRENCIES = ['USD', 'GBP']
+    CONVERSION_RATES = {
+                        'USD': {
+                             'GBP' : 0.65
+                                }, 
+                        'GBP': {
+                             'USD': 1.53
+                                }
+                       }
 
     def __init__(self, currency, value = 0):
         if currency in self.AVAILABLE_CURRENCIES:
@@ -9,3 +20,13 @@ class Amount(object):
         
     def __str__(self):
         return "%s%s" % (self.currency, int(self.value))
+
+    def getValueConverted(self, to_currency):
+        if to_currency == self.currency:
+            return self.value
+        elif to_currency in self.AVAILABLE_CURRENCIES:
+            logger.debug("Converted with Rate: " + str(self.CONVERSION_RATES[self.currency][to_currency]))
+            return self.value * self.CONVERSION_RATES[self.currency][to_currency]
+        else:
+            logger.warning("The currency %s is not available [%s]", to_currency, ', '.join(self.AVAILABLE_CURRENCIES))
+            return 0
