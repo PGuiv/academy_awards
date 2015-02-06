13f5b86959cc7fccbec7cb6ca908c38a20df44d1
Pierre Guivarch <pierre@crestandco.com>
2015-02-06 09:28:30 -0500

    Add customLog

---
+++ main/customLog.py
@@ -0,0 +1,18 @@
+import logging
+
+def setup_custom_logger(name, log_level = logging.DEBUG):
+    logging.basicConfig(level=log_level)
+
+    logger = logging.getLogger(name)
+
+    # create a file handler
+    handler = logging.FileHandler('../logs/main.log')
+    handler.setLevel(logging.DEBUG)
+
+    # create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(handler)
+
+    return logger
