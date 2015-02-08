6e40ef7bf1f40432fcda8e090bda90ecbcf5b2b6
Pierre Guivarch <pierre@crestandco.com>
2015-02-08 14:06:44 -0500

    Setup logger level on init

--- main/CustomLog.py
+++ main/CustomLog.py
@@ -2,6 +2,14 @@ import os
 import logging
 
 def setup_custom_logger(name, log_level = logging.DEBUG):
+    
+    if log_level == 'INFO':
+        log_level = logging.INFO
+    elif log_level == 'WARNING':
+        log_level = logging.WARNING
+    else:
+        log_level = logging.DEBUG
+
     logging.basicConfig(level=log_level)
 
     logger = logging.getLogger(name)
@@ -11,13 +19,21 @@ def setup_custom_logger(name, log_level = logging.DEBUG):
     if not os.path.isdir(log_dirs):
         os.mkdir(log_dirs)
 
-    handler = logging.FileHandler(os.path.join(os.path.dirname( __file__ ), '..', 'logs', 'main.log'))
-    handler.setLevel(logging.DEBUG)
-
     # create a logging format
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+
+    # Log in the file
+    fh = logging.FileHandler(os.path.join(os.path.dirname( __file__ ), '..', 'logs', 'main.log'))
+    fh.setLevel(log_level)
+    fh.setFormatter(formatter)
+
+    # Log in the file
+    sh = logging.StreamHandler()
+    sh.setLevel(log_level)
+    sh.setFormatter(formatter)
+
     # add the handlers to the logger
-    logger.addHandler(handler)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
 
     return logger
